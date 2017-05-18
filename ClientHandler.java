58f6abc2e05f707457ebd3a3f0f65544813ea835
mirkocolombo1994 <mirko5.colombo@mail.polimi.it>
2017-05-18 14:13:43 +0200

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -15,28 +15,28 @@ public class ClientHandler extends Thread {
 
     public ClientHandler(Socket socket) throws IOException {
         mSocket = socket;
-        mInputStream = socket.getInputStream();
-        mOutputStream = socket.getOutputStream();
+        mInputStream = socket.getInputStream(); //serve per aprire il canale di comunicazione in ingresso
+        mOutputStream = socket.getOutputStream(); //serve per aprire il canale di comunicazione in uscita
     }
 
     @Override
-    public void run() {
+    public void start() {
         try {
-            // creiamo i wrapper
-            ObjectInputStream scanner = new ObjectInputStream(mInputStream);
-            ObjectOutputStream printer = new ObjectOutputStream(mOutputStream);
-            printer.flush();
+            // creiamo i wrapper... COSA SONO???? Sono i cosi per serializzare gl ioggetti, involucri che contengono roba serializzata
+            ObjectInputStream scanner = new ObjectInputStream(mInputStream); //Serve per de-serializzare la stringa di byte che gli arriva; ossia ricostruisce l'oggetto
+            ObjectOutputStream printer = new ObjectOutputStream(mOutputStream); //Serve per serializzare l'oggetto trasformandolo in una sequenza di byte
+            printer.flush(); //serve per pulire quello che è eventualmente rimasto in uscita
             // implementiamo la logica
             boolean exit = false;
             while (!exit) {
                 // leggiamo cosa ci dice il client (ipotizziamo che il protocollo stabilisca che il comando sia una stringa)
                 String command = null;
                 try {
-                    command = (String) scanner.readObject();
-                } catch (ClassNotFoundException e) {
+                    command = (String) scanner.readObject(); //scanner è il wrapper di object, e quindo leggo quello che mi arriva
+                } catch (ClassNotFoundException e) { //Errore in caso non arrivi una stringa
                     command = "wtf";
                 }
-                System.out.println("Ho letto: " + command);
+                System.out.println("Ho letto: " + command); 
                 switch (command) {
                     case "sum":
                         // leggo due interi
@@ -45,8 +45,8 @@ public class ClientHandler extends Thread {
                         int c = a + b;
                         System.out.println("Somma: a = " + a + ", b = " + b + ", c = " + c);
                         // invio la somma al client
-                        printer.writeInt(c);
-                        printer.flush();
+                        printer.writeInt(c); //Scrivo un intero, quindi serializzali come un intero
+                        printer.flush(); //sempre per pulire
                         break;
                     case "multiply":
                         // leggo due interi
@@ -68,9 +68,10 @@ public class ClientHandler extends Thread {
                 }
             }
             // chiudo tutto
-            scanner.close();
-            printer.close();
-            mSocket.close();
+            //Qui è importante che il socket lo chiudi per ultimo!!
+            scanner.close(); //Chiudo input
+            printer.close(); //chiudo output
+            mSocket.close(); //chiudo connessione
         } catch (IOException e) {
             e.printStackTrace();
         }
